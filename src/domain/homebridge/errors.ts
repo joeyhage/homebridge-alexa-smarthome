2d7aa0af9c80cec6e959f4e84316832407ed539c
Joey Hage <3220319+joeyhage@users.noreply.github.com>
2023-09-16 14:38:08 -0500

    feat: print all device names and types connected to Alexa account (#39)

--- src/domain/homebridge/errors.ts
+++ src/domain/homebridge/errors.ts
@@ -1,24 +1,24 @@
 export abstract class PluginError extends Error {
-  constructor(message: string, public readonly name: string) {
+  constructor(message: string, public readonly name: string, public readonly cause?: unknown) {
     super(message);
     this.message = `${name}(${message})`;
   }
 }
 
 export class JsonFormatError extends PluginError {
-  constructor(message: string) {
-    super(message, JsonFormatError.name);
+  constructor(message: string, cause?: unknown) {
+    super(message, JsonFormatError.name, cause);
   }
 }
 
 export class ValidationError extends PluginError {
-  constructor(message: string) {
-    super(message, ValidationError.name);
+  constructor(message: string, cause?: unknown) {
+    super(message, ValidationError.name, cause);
   }
 }
 
 export class IoError extends PluginError {
-  constructor(message: string) {
-    super(message, IoError.name);
+  constructor(message: string, cause?: unknown) {
+    super(message, IoError.name, cause);
   }
 }
--- src/util/plugin-logger.test.ts
+++ src/util/plugin-logger.test.ts
@@ -1,6 +1,7 @@
 import { PlatformConfig } from 'homebridge';
 import { DeviceOffline } from '../domain/alexa/errors';
 import { PluginLogger } from './plugin-logger';
+import { JsonFormatError } from '../domain/homebridge/errors';
 
 describe('errorT', () => {
   test('should log DeviceOffline at info level when debug enabled', () => {
@@ -33,5 +34,20 @@ describe('errorT', () => {
     // then
     expect(mockLogger.error).toHaveBeenCalledTimes(1);
     expect(mockLogger.error).toHaveBeenCalledWith('TEST - fake error');
+  });
+
+  test('should log error and cause', () => {
+    // given
+    const mockLogger = global.MockLogger;
+    const logger = new PluginLogger(mockLogger, {
+      debug: true,
+    } as unknown as PlatformConfig);
+
+    // when
+    logger.errorT('TEST', new JsonFormatError('bad json', new Error('an example cause')))();
+
+    // then
+    expect(mockLogger.error).toHaveBeenCalledTimes(1);
+    expect(mockLogger.error).toHaveBeenCalledWith('TEST - JsonFormatError(bad json). Caused by - an example cause');
   });
 });
